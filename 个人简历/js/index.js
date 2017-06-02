a084f6ec743ed8aa4729fdd67915e167a3fb12c2
liheling <1034029229@qq.com>
2017-06-03 03:03:00 +0800

    个人简历

---
+++ 个人简历/js/index.js
@@ -0,0 +1,42 @@
+var count = 0;
+var page = document.getElementsByTagName("page");
+var page1 = page[0],	
+    page2 = page[1],
+    page3 = page[2],
+    page4 = page[3];
+page1.onclick = function() {
+    count++;
+    if(count == 1) {
+        page2.classList.add("page2_anim-1");
+    }
+}
+page2.onclick = function() {
+    count++;
+    if(count == 2) {
+        page3.classList.add("page3_anim-1");
+    }
+}
+page3.onclick = function() {
+    count++;
+    if(count == 3) {
+        page4.classList.add("page4_anim-1");
+    }
+}
+document.oncontextmenu = function(event) {
+    event.preventDefault();
+    if(count == 0) {
+        return;
+    }
+    else {
+        count--;
+    }
+    if(count == 0) {
+        page2.classList.remove("page2_anim-1");
+    }
+    if(count == 1) {
+        page3.classList.remove("page3_anim-1");
+    }
+    if(count == 2) {
+        page4.classList.remove("page4_anim-1");
+    }
+}
